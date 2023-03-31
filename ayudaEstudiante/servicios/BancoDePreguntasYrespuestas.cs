be5fbd959e2042f4a5947b249ab3747d01a7e0ae
fantasma mv <83436765+fantasmagg@users.noreply.github.com>
2023-03-30 23:00:24 -0500

    Version2

--- ayudaEstudiante/servicios/BancoDePreguntasYrespuestas.cs
+++ ayudaEstudiante/servicios/BancoDePreguntasYrespuestas.cs
@@ -11,14 +11,34 @@ namespace ayudaEstudiante.servicios
         public string preguntas (SenderMensaje senderMensaje)
         {
 
-            if (senderMensaje.MessageTextSend.Equals("hola", StringComparison.InvariantCultureIgnoreCase) )
+            if (senderMensaje.MessageTextSend.Equals("hola", StringComparison.InvariantCultureIgnoreCase) ||
+                senderMensaje.MessageTextSend.Equals("ola", StringComparison.InvariantCultureIgnoreCase) || 
+                senderMensaje.MessageTextSend.Equals("hol", StringComparison.InvariantCultureIgnoreCase) || 
+                senderMensaje.MessageTextSend.Equals("hi", StringComparison.InvariantCultureIgnoreCase) || 
+                senderMensaje.MessageTextSend.Equals("hello", StringComparison.InvariantCultureIgnoreCase) || 
+                senderMensaje.MessageTextSend.Equals("holasss", StringComparison.InvariantCultureIgnoreCase)
+                )
             {
                 return @"Hola Soy un bot un tanto limitado pero espero poder adudarte:
-                        <br>Ver pesunl
+                        <br>1 Ver pensum
                         <br>Duracion de Carreras
                         <br>Correos del itla
                         <br>Numeros de telefonos";
             }
+
+            if (senderMensaje.MessageTextSend.Equals("Ver pensum", StringComparison.InvariantCultureIgnoreCase) || 
+                senderMensaje.MessageTextSend.Equals("1", StringComparison.InvariantCultureIgnoreCase)||
+                senderMensaje.MessageTextSend.Equals("ver pensum", StringComparison.InvariantCultureIgnoreCase) ||
+                senderMensaje.MessageTextSend.Equals("ber pensul", StringComparison.InvariantCultureIgnoreCase) ||
+                senderMensaje.MessageTextSend.Equals("pensul", StringComparison.InvariantCultureIgnoreCase) ||
+                senderMensaje.MessageTextSend.Equals("pensum", StringComparison.InvariantCultureIgnoreCase)
+                
+                )
+            {
+                return @"<a class='ss' href='https://itla.edu.do/images/pdf/pensum-seguridad-informatica-2020.pdf'  target='_blank' rel='alternate noreferrer'>Segurida</a>";
+            }
+
+
             return "...";
 
         }
